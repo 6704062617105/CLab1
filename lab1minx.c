6723d4b941e01586df380f30e0359bb58cceee72
6704062617105 <s6704062617105@email.kmutnb.ac.th>
2024-11-28 17:57:23 +0700

    Version 3 use scanf to input data from user

--- lab1minx.c
+++ lab1minx.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main(){
-	int num1 = 10,num2 = 5;
+	int num1,num2;
+	
+	scanf("%d %d",&num1,&num2);
 	
 	if(num1 > num2) 
-		printf("num1 id greater than num2");
+		printf("num1 id greater than num2 = %d",num1-num2);
 	else
-		printf("num2 is greater than num1");
+		printf("num2 is greater than num1 = %d",num2-num1);
 	
 }
